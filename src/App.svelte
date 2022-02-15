fd9c86b2c1dcb399c1608b38696d36d60263dc8e
Millie Cheung <cheungmillie9@gmail.com>
2022-02-15 13:43:10 +0100

    simplifed file name

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
 <script>
 	import Header from './components/Header.svelte'
 	import Footer from './components/Footer.svelte'
-	import ExampleOne from './components/ExampleOne.svelte'
-	import ExampleTwo from './components/ExampleTwo.svelte'
-	import ExampleThree from './components/ExampleThree.svelte'
-	import ExampleFour from './components/ExampleFour.svelte'
-	import ExampleFive from './components/ExampleFive.svelte'
+	import Ex1 from './components/Ex1.svelte'
+	import Ex2 from './components/Ex2.svelte'
+	import Ex3 from './components/Ex3-1.svelte'
+	import Ex4 from './components/Ex4-1.svelte'
+	import Ex5 from './components/Ex5.svelte'
 	import Tabs from './shared/Tabs.svelte'
 
 	// Menu
 	let menuItems = ['Component Structure', 'Binding: Text/Group Inputs', 'Props', 'Slots', 'Each Blocks'];
-    let activeItem = menuItems[4];
+    let activeItem = menuItems[0];
 
 	const tabChange = (e) => {
 		activeItem = e.detail;
@@ -21,15 +21,15 @@
 <main>
 	<Tabs {activeItem} {menuItems} on:tabChange={tabChange} />
     {#if activeItem === menuItems[0]}
-        <ExampleOne />
+        <Ex1 />
 	{:else if activeItem === menuItems[1]}
-		<ExampleTwo />
+		<Ex2 />
 	{:else if activeItem === menuItems[2]}
-		<ExampleThree />
+		<Ex3 />
 	{:else if activeItem === menuItems[3]}
-		<ExampleFour />
+		<Ex4 />
 	{:else if activeItem === menuItems[4]}
-		<ExampleFive />
+		<Ex5 />
 	{/if}
 </main>
 <Footer />
---
+++ src/components/Ex3-1.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Ex3child from './Ex3-2.svelte'; // passing data down to child
+
+    let imgSrc = '/images/burger.svg';
+    let imgAlt = 'hamburger';
+</script>
+
+<div class="container">
+    <div class="code">
+        <Ex3child answer={'burgers'} {imgSrc} {imgAlt} />
+    </div>
+    
+    <div class="output">
+        {`<-------- PARENT component -------->`}<br/><br/>
+        {`<script>`}<br/>
+        <span class="span-ml">{`import Ex3child from './Ex3-2.svelte';`}</span><br/><br/>
+        <span class="span-ml">{`let imgSrc = '/images/burger.svg';`}</span><br/>
+        <span class="span-ml">{`let imgAlt = 'hamburger';`}</span><br/>
+        {`</script>`}<br/><br/>
+
+        {`<Ex3child answer={'burgers'} {imgSrc} {imgAlt} />`}<br/><br/>
+
+        {`<-------- CHILD component -------->`}<br/><br/>
+        {`<script>`}<br/>
+        <span class="span-ml">{`export let answer;`}<br/></span>
+        <span class="span-ml">{`export let imgSrc;`}<br/></span>
+        <span class="span-ml">{`export let imgAlt;`}<br/></span>
+        {`</script>`}<br/><br/>
+        {`<h2>My favorite food is {answer}. Here is a picture of it...</h2>`}<br/>
+        {`<img src={imgSrc} alt={imgAlt}>`}<br/><br/>
+        {`<style>`}<br/>
+        <span class="span-ml">{`img { width: 10rem; }`}<br/></span>
+        {`</style>`}<br/><br/>
+    </div>
+</div>
